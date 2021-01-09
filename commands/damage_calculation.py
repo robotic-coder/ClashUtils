2cc362dcddede5747d51c6a98b99a61e8d705163
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-01-09 18:59:25 +0200

    Add native slash command for damage calculation

--- commands/damage_calculation.py
+++ commands/damage_calculation.py
@@ -3,24 +3,29 @@ import discord.ext.commands
 import re
 from commands.utils.helpers import *
 import commands.utils.emojis as emojis
+from discord_slash import SlashCommand, SlashContext
 
 earthquake_damage = ( 0.145, 0.17, 0.21, 0.25, 0.29 )
 lightning_damage = ( 150, 180, 210, 240, 270, 320, 400, 480, 560 )
 shield_damage = ( 1260, 1460, 1660, 1860, 1960 )
 
 @discord.ext.commands.command(
+	name = "damage",
 	description = "Calculates damage from Lightning/Earthquake spells and the Royal Champion's Seeking Shield ability.",
 	brief = "Calculates damage from Lightning/Earthquake spells and the Royal Champion's Seeking Shield ability.",
 	usage = "[building hp] [qty]x [item][level], [qty]x [item][level]...",
 	help = "4800 6x lightning8, 1x earthquake5, 1x shield5, 1x lightning9"
 )
-async def damage(ctx: discord.ext.commands.Context, *args):
+async def damage_standard(ctx: discord.ext.commands.Context, *args):
 	params = re.match("(\d+) (((\d+)x ((earthquake([1-5]))|(lightning([1-9]))|(shield([1-5])))((, )|$))+)", " ".join(args))
 	if params is None:
-		await send_command_help(ctx, damage)
+		await send_command_help(ctx, damage_standard)
 		return
 	
-	building_hp = params.group(1)
+	(embed, content) = damage(params.group(1), params.group(2))
+	ctx.channel.send(content, embed=embed)
+
+def damage(building_hp, spell_list):
 	total_damage = 0
 	earthquake_count = 0
 	shield_count = 0
@@ -28,10 +33,9 @@ async def damage(ctx: discord.ext.commands.Context, *args):
 
 	embed = discord.Embed()
 
-	spells = params.group(2).split(", ")
+	spells = spell_list.split(", ")
 	if len(spells) >= 14:
-		await ctx.send("You can't use more than 14 spells!")
-		return
+		return ([], "You can't use more than 14 spells!")
 	
 	for spell in spells:
 		spell_params = re.match("^(\d+)x ([A-Za-z]*)(\d+)$", spell)
@@ -68,16 +72,38 @@ async def damage(ctx: discord.ext.commands.Context, *args):
 					damage = str(shield_damage[level-1])+" points"
 					embed.add_field(name=str(emojis.heroes["champion"])+" Seeking Shield level "+str(level), value=damage, inline=False)
 				else:
-					await ctx.channel.send("You can't use Seeking Shield twice!")
-					return
+					return ([], "You can't use Seeking Shield twice!")
 	if earthquake_count > 1:
 		embed.set_footer(text="This result may be off by a few points, because the fine details of how Clash combines damage from multiple Earthquake Spells (for example, how much rounding is used) is unknown.")
 	
 	if float(total_damage) >= float(building_hp): marker = "✅"
 	else: marker = "❌"
 
-	embed.title = "Damage Calculation"
-	await ctx.channel.send("Total: "+round_fixed(total_damage, 2)+" points "+marker, embed=embed)
+	embed.title = "Damage Breakdown"
+	return ([embed], "Total: "+round_fixed(total_damage, 2)+" points "+marker)
 
 def setup(bot: discord.ext.commands.Bot):
-	bot.add_command(damage)
+	bot.add_command(damage_standard)
+	bot.slash.add_slash_command(damage_slash,
+		name="damage",
+		description="Calculates damage from Lightning/Earthquake Spells and the Royal Champion's Seeking Shield",
+		guild_ids=bot.command_guild_ids,
+		options=[{
+			"type": 4,
+			"name": "building-hp",
+			"description": "The hitpoints of the target building",
+			"required": True
+		},
+		{
+			"type": 3,
+			"name": "items",
+			"description": "Each item must be in the format [qty]x [item][level] and separated by a comma and space.",
+			"required": True
+		}]
+	)
+
+async def damage_slash(ctx: SlashContext, building_hp, spells):
+	await ctx.send(content="Note: This feature is in beta, and not all `//` commands are supported yet.")
+	
+	(embeds, content) = damage(building_hp, spells)
+	await ctx.send(content=content, embeds=embeds)
