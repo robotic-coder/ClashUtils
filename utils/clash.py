05603f25fcbea31f0f65966e809eac34a5002fb5
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-04-06 22:59:55 +0000

    Always use new War class

--- utils/clash.py
+++ utils/clash.py
@@ -18,7 +18,7 @@ class Clash(coc.Client):
 				war = await super().get_current_war(clan_tag, cwl_round=coc.WarRound.current_preparation, cls=cls, **kwargs)
 			elif group is not None and war.state == "warEnded" and not (len(group.rounds) == group.number_of_rounds and war.war_tag in group.rounds[-1]):
 				# if in cwl, the war has ended, and war is not the last round of the week: check the latest round to see if it is out of prep (in battle or ended)
-				next_war = (await self.get_league_wars(war.league_group.rounds[-1], clan_tag=clan_tag).flatten())[0]
+				next_war = (await self.get_league_wars(war.league_group.rounds[-1], clan_tag=clan_tag, cls=cls).flatten())[0]
 				if next_war.state != "preparation":
 					next_war.league_group = war.league_group # wars from get_league_wars() do not have a cwl group, so attach the previously-fetched group
 					war = next_war
